5b493faa60b7915aae751b3eea971a64347b663c
DavideM10 <davidemiotti30@gmail.com>
2025-03-20 10:35:47 +0000

    Aggiungi endpoint per eseguire codice Python e aggiorna interfaccia utente

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
 from flask import Flask, request, jsonify
-import pyautogui as py
 import pywhatkit as kit
-import time
 import random
 
 app = Flask(__name__)
 
 @app.route('/run-python', methods=['POST'])
 def run_python():
-    fabio = "+393397701251"
-    degan = "+393312821650"
     passone = "+393760049643"
-    id = "JX45yP9rzySFoIHNcl6EFq"
-    count = 0
-
     msg = ["Ciao, questo è un messaggio automatico.",
            "Se hai ricevuto questo messaggio, significa che il mio script funziona correttamente.",
            "Se hai bisogno di aiuto, non esitare a contattarmi.",
@@ -27,13 +20,7 @@ def run_python():
            "Sei un genio!",
            ]
 
-    kit.sendwhatmsg_instantly(fabio, random.choice(msg))
-
-    while count <= 3:
-        a = random.choice(msg)
-        py.typewrite(a)
-        py.press("enter")
-        count += 1
+    kit.sendwhatmsg_instantly(passone, random.choice(msg))
 
     return jsonify({"status": "success"})
 
